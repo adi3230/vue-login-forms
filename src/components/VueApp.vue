3f4c50271f7c4fdad40d6367b4b04b8355735de1
Aditya Joshi <adi3230@gmail.com>
2018-09-08 01:06:01 +0200

    style(): holygrail layout setup

---
+++ src/components/VueApp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="hg-vueapp">
+        <header class="hg-vueapp-header">
+            Header
+        </header>
+  
+        <aside class="hg-vueapp-sidebar-left">
+            Left Sidebar
+        </aside>
+
+        <article class="hg-vueapp-content">
+            <div class="hg-vueapp-content__image"><img alt="Vue logo" src="../assets/logo.png"></div>
+        </article>
+
+        <aside class="hg-vueapp-sidebar-right">
+            Right Sidebar
+        </aside>
+
+        <footer class="hg-vueapp-footer">
+            Footer
+        </footer>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+    .hg-vueapp-content {
+        &__image {
+            text-align: center;
+        }
+    }
+</style>
+
+
