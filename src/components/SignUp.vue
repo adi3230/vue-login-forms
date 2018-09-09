35682be9ea3d9ba7646ba08deefa6beb69374caa
Aditya Joshi <adi3230@gmail.com>
2018-09-09 15:32:26 +0200

    feat(signup): signup component, styles

---
+++ src/components/SignUp.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="vueapp-signup">
+        <h3>Sign In</h3>
+        <div class="vueapp-signup">
+            <input class="vueapp-signup__form-field" type="text" placeholder="Email"/><br/>
+            <input class="vueapp-signup__form-field" type="text" placeholder="Password"/><br/>
+            <button class="vueapp-signup__form-cta">SignUp</button>
+        </div>
+        <p>If you already have an account? You can <router-link to='/login'>login</router-link> here</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signUp',
+    data: function() {
+        return {}
+    },
+    methods: {}
+}
+</script>
