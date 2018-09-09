3828f78293162751dcfc1bfa0a3bfc86874a147a
Aditya Joshi <adi3230@gmail.com>
2018-09-09 22:14:50 +0200

    feat(store): import store , setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import firebase from 'firebase'
 
 import './assets/app.scss'
@@ -21,17 +22,14 @@ Vue.config.productionTip = false
 
   firebase.initializeApp(config)
 
-  firebase.auth().onAuthStateChanged((user => {
-    if (!app) {
-      app = new Vue({
-          el: '#app',
-          router,
-          render: h => h(App)
-      })
-}
-  }))
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+  firebase.auth().onAuthStateChanged((user) => {
+      if (!app) {
+        app = new Vue({
+            el: '#app',
+            router,
+            store,
+            render: h => h(App)
+        })
+        if(user) store.dispatch('autoSignIn', user)
+      }
+  })
