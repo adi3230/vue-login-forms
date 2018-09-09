6daef1611c41d329f0500bef640f645323a1e271
Aditya Joshi <adi3230@gmail.com>
2018-09-09 15:30:37 +0200

    chore(): add router, wrapper vueapp component, minor changes global, import styles

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import './assets/_global.scss'
+import './assets/login.scss'
+import './assets/signUp.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
