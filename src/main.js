9acee817853f3ba396a686003aed2e156316580d
Aditya Joshi <adi3230@gmail.com>
2018-09-09 16:52:52 +0200

    chore(firebase): firebase setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 import './assets/_global.scss'
 import './assets/login.scss'
 import './assets/signUp.scss'
 
 Vue.config.productionTip = false
+
+  let app;
+  // Initialize Firebase
+  // Enter your own created config
+  var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  };
+
+  firebase.initializeApp(config)
+
+  firebase.auth().onAuthStateChanged((user => {
+    if (!app) {
+      app = new Vue({
+          el: '#app',
+          router,
+          render: h => h(App)
+      })
+}
+  }))
 
 new Vue({
   router,
