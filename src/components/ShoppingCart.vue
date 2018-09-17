d4e8dd2f8d980e5544480885c8e2bccbf9415f88
Aditya Joshi <adi3230@gmail.com>
2018-09-17 23:49:40 +0200

    feat(shoppingcart): display purchased product

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Shopping Cart</h1>
+        <ul>
+            <li v-for="product in getProducts" :key="product.id">
+                {{product.title}} - {{product.price | currency}} - {{product.quantity}}
+
+            </li>
+        </ul>
+        <p>Total: {{ getTotal | currency }}</p>
+    </div>
+</template>
+<script>
+export default {
+    computed: {
+        getProducts() {
+            return this.$store.getters.getCartProducts;
+        },
+        getTotal() {
+            return this.$store.getters.getCartTotal;
+        }
+    },
+}
+</script>
+
--- src/components/ShoppingComponent.vue
+++ src/components/ShoppingComponent.vue
@@ -2,14 +2,18 @@
     <div>
         <h1>Welcome to Shopping using Vuex</h1>
         <ProductList/>
+        <hr/>
+        <ShoppingCart/>
     </div>
 </template>
 <script>
 import ProductList from '@/components/ProductList'
+import ShoppingCart from '@/components/ShoppingCart'
 
 export default {
     components: {
-        ProductList
+        ProductList,
+        ShoppingCart
     }
 }
 </script>
